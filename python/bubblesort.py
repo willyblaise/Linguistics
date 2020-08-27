91ff42e9925e889758390ef7c30c712b56ae058c
willyblaise <47799036+willyblaise@users.noreply.github.com>
2020-08-27 00:31:01 -0500

    Update bubblesort.py

--- python/bubblesort.py
+++ python/bubblesort.py
@@ -7,3 +7,15 @@ def bsort(listn):
 				lnums[i] = listn[j]
 				lnums[j] = temp
 				print("after: ", i, listn[i], j, listn[j])
+
+				
+				
+for i in range(len(blist)):
+     for j in range(i+1, len(blist)):
+             print(i, blist[i], j, blist[j])
+             if blist[i] > blist[j]:
+                     temp = blist[i]
+                     blist[i] = blist[j]
+                     blist[j] = temp
+	print("after", blist[i], blist[j])
+				
