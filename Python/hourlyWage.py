b3e20f58339fb9a0c0fa58ba33993b3a5999291d
William <kravendahunter@hotmail.com>
2021-02-07 19:10:58 -0600

    adding main method kind of

--- Python/hourlyWage.py
+++ Python/hourlyWage.py
@@ -11,5 +11,5 @@ def hourly_wage():
     hourly_wage = salary / 2080
     return print("Your Hourly Wage is {}".format(hourly_wage))
 
-
-hourly_wage()
+if __name__ == "__main__":
+    hourly_wage()
