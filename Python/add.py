dd5eae3f9a74b18451243114c5b0dcd974be226a
William <kravendahunter@hotmail.com>
2021-03-13 22:16:49 -0600

    making the add.py program a little better

--- Python/add.py
+++ Python/add.py
@@ -2,9 +2,19 @@
 
 import sys
 
-sum = 0
+name = sys.argv[0]
 
-for number in range(1, len(sys.argv)):
-    sum += int(sys.argv[number])
 
-print(sum)
+def summed_up():
+    global sum
+    for number in range(1, len(sys.argv)):
+        sum += int(sys.argv[number])
+
+    print(f"{sum} is the sum of all passed in Arguments")
+
+
+if __name__ == "__main__":
+
+    print(f"{name.upper()} is the name of this application.")
+    sum = 0
+    summed_up()
