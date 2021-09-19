7947eae731253987aca737a82b63f6af7bceb802
William <kravendahunter@hotmail.com>
2021-09-18 22:49:09 -0500

    adding more complicated fib function

--- Python/fib.py
+++ Python/fib.py
@@ -1,6 +1,9 @@
 from functools import lru_cache
 from functools import reduce
 
+fibonacci_cache = {}
+
+
 @lru_cache(maxsize=1000)
 def fibonacci(n):
     if n == 1:
@@ -11,11 +14,28 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 
+def fibonacci_long(n):
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 1
+    elif n > 2:
+        value = fibonacci_long(n-1) + fibonacci_long(n-2)
+
+    fibonacci_cache[n] = value 
+    return value 
+
 for n in range(1, 500):
     print(n, ":", fibonacci(n))
 
+print("Fibonacci Long now\n")
 
-lis = [1,2,3,4,5,6]
-print("\n")
-sum = reduce(lambda x,y : x+y, lis)
-print(f"The sum reduced is: {sum}")
+for n in range(1,1000):
+    print(n, ":", fibonacci_long(n))
+#lis = [1,2,3,4,5,6]
+#print(" \n")
+#sum = reduce(lambda x,y : x+y, lis)
+#print(f"The sum reduced is: {sum}")
