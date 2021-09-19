eaba47e0d7f733c1cfde85d1a636b8545dd8d5dd
William <kravendahunter@hotmail.com>
2021-09-18 23:52:07 -0500

    added logging

--- Python/vs.py
+++ Python/vs.py
@@ -1,14 +1,34 @@
 import sys
 import os
 import requests
+import time
+import logging
+import datetime
 
-req = requests.get(f'https://wttr.in/{sys.argv[1].replace(" ", "+")}')
+
+#creating a logger
+logging.basicConfig(filename = "/home/champ/problems.log", level = logging.DEBUG)
+logger = logging.getLogger()
+
+
+start = time.time()
+try:
+    req = requests.get(f'https://wttr.in/{sys.argv[1].replace(" ", "+")}')
+except:
+    logger.error(f"{datetime.datetime.now()} - Maybe no city was passed into the program.")
+    raise
+finally:
+    stop = time.time()
+    dt = stop - start
+    logger.info(f"it took {dt} to run the process.")
+
+
 
 print(req.text)
 print(f"{req.status_code}")
 
 
-stax = lambda x : x * 1.0825
+#stax = lambda x : x * 1.0825
 
-subtotal = int(input("Please provide the Subtotal: "))
-print(f"Subtotal + Sales Tax is: {stax(subtotal)}")
+#subtotal = int(input("Please provide the Subtotal: "))
+#print(f"Subtotal + Sales Tax is: {stax(subtotal)}")
