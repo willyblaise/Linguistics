c21a997fa8853ae49f21742efb395355e9abef62
William <kravendahunter@hotmail.com>
2021-09-19 22:40:33 -0500

    added logging to percentage change and changed name of vs.py to something more useful

--- Python/percentage_change.py
+++ Python/percentage_change.py
@@ -1,11 +1,27 @@
+import time
+import logging
+import datetime
+
+logging.basicConfig(filename = "/home/champ/problems2.log", level = logging.DEBUG)
+logger = logging.getLogger()
 
 
 def pchg():
     cv = int(input("Please provide the current value: "))
     iv = int(input("Please provide the initial value: "))
-    pc = (cv - iv)/iv * 100
-    return print("The percentage change is {}".format(pc))
+
+    c_minus_i = cv - iv
+    try:
+        c_minus_i = c_minus_i / iv
+    except ZeroDivisionError:
+        logger.error(f"{datetime.datetime.now()} - You cannot divide by zero")
+        raise
+    finally:
+        logger.info(f"This app finished at {datetime.datetime.now()}")
+
+    return c_minus_i * 100
 
 
 if __name__ == "__main__":
-    pchg()
+    pc = pchg()
+    print("The percentage change is {}".format(pc))
