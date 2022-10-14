445e8f944b89a8c55f62b298982bc908f8886bc2
Willem <kravendahunter@hotmail.com>
2022-10-13 22:09:25 -0500

    updating to add classes to cool

--- Python/cool.py
+++ Python/cool.py
@@ -2,6 +2,16 @@
 
 import sqlite3
 import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class Medication:
+    patient_id: int = 0
+    units: int = 0
+    meal: str = "nothing to eat"
+
+
 
 conn = sqlite3.connect("/home/champ/sqlite/cool.db")
 
@@ -17,11 +27,28 @@ def data_entry():
 
 # When Your Input Might Raise an Exception
 # Use try and except to detect when the user enters data that can't be parsed.
+    cool = Medication()
+
 
     while True:
         try:
             # Note: Python 2.x users should use raw_input, the equivalent of 3.x's input
-            patient = int(input("Please enter your Patient: "))
+            #patient = int(input("Please enter your Patient: "))
+            cool.patient_id = int(input("Please enter your Patient: "))
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+            #better try again... Return to the start of the loop
+            continue
+        else:
+            print("Patient number was successfully parsed!")
+            print("we're ready to exit the loop.")
+            break
+
+    while True:
+        try:
+            # Note: Python 2.x users should use raw_input, the equivalent of 3.x's input
+            #units = int(input("Please provide units: "))
+            cool.units = int(input("Please provide units: "))
         except ValueError:
             print("Sorry, I didn't understand that.")
             #better try again... Return to the start of the loop
@@ -31,23 +58,10 @@ def data_entry():
             #we're ready to exit the loop.
             break
 
-    while True:
-        try:
-            # Note: Python 2.x users should use raw_input, the equivalent of 3.x's input
-            units = int(input("Please provide units: "))
-        except ValueError:
-            print("Sorry, I didn't understand that.")
-            #better try again... Return to the start of the loop
-            continue
-        else:
-            #age was successfully parsed!
-            #we're ready to exit the loop.
-            break
 
+    cool.meal = input("What did Cool eat? ")
 
-    meal = input("Did Cool eat? ")
-
-    c.execute("INSERT INTO inject (Units, Meal, subjectId) values(?,?,?)", (units, meal, patient))
+    c.execute("INSERT INTO inject (Units, Meal, subjectId) values(?,?,?)", (cool.units, cool.meal, cool.patient_id))
 
     conn.commit()
 
@@ -62,7 +76,7 @@ if __name__ == "__main__":
 
     while True:
         try:
-            injections = int(input("How many injections do you want to input?"))
+            injections = int(input("How many injections do you want to input? "))
         except ValueError:
             print("Sorry, I don't understand that.")
             continue
