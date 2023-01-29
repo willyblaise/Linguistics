6e85b9609afe236713f0778fb5671f55250a5673
Willem <kravendahunter@hotmail.com>
2023-01-29 01:36:50 -0600

    adding yt thing

---
+++ Python/yt.py
@@ -0,0 +1,15 @@
+import pytube
+
+
+link = input("Please enter a link so we can get busy: ")
+yt = pytube.YouTube(link)
+
+print("Title:", yt.title)
+print("Author:", yt.author)
+print("Published date:", yt.publish_date.strftime("%Y-%m-%d"))
+print("Number of views:", yt.views)
+print("Length of video:", yt.length, "seconds")
+
+yt.streams.get_highest_resolution().download("~/Videos")
+
+print("Video successfullly downloaded from", link)
