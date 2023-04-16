eddea0608a7c57d23ad5d258a0d584b64bc71581
Willem <kravendahunter@hotmail.com>
2023-04-16 09:34:50 -0500

    making sql more sophisticated

---
+++ Python/Sqlite/cool_select.py
@@ -0,0 +1,39 @@
+import sqlite3
+import sys
+from pathlib import Path
+
+conn = sqlite3.connect("cool.db")
+
+c = conn.cursor()
+
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS "inject" (id integer primary key autoincrement, Units integer, Meal BOOLEAN, "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP, "subjectId" integer, CONSTRAINT fk_name FOREIGN KEY (subjectId) REFERENCES subject(id))')
+
+def data_entry():
+    patient  = input("Please provide a ID: ")
+    units    = input("Please provide units: ")
+    meal     = input("Did Cool eat? ")
+
+    c.execute("INSERT INTO inject (Units, Meal, subjectId) values(?,?,?)", (units, meal, patient))
+
+    conn.commit()
+
+def select_table():
+#    c.execute("SELECT * FROM inject")
+    rsql = Path("sql/inject.sql").read_text()
+    c.execute(rsql)
+
+
+if __name__ == "__main__":
+
+    select_table()
+    results = c.fetchone()
+    all_results = c.fetchall()
+#   print(f"{all_results}")
+
+    for row in all_results:
+        print(row)
+
+    c.close()
+    conn.close()
---
+++ Python/Sqlite/inject.sql
@@ -0,0 +1,6 @@
+
+
+SELECT id, units, meal, created_at
+FROM inject
+order by created_at
+DESC LIMIT 10;
