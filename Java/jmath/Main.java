0161be5ea9c4b9ce1551c193f1cbd122c9ae9beb
willyblaise <kravendahunter@hotmail.com>
2023-11-25 04:13:04 -0600

    updating to add remainder

--- Java/jmath/Main.java
+++ Java/jmath/Main.java
@@ -8,16 +8,21 @@ public class Main {
         calculator.addOperation("subtract", new Subtraction());
         calculator.addOperation("multiply", new Multiplication());
         calculator.addOperation("divide", new Division());
+        calculator.addOperation("mod", new Modulus());
 
         // Use the calculator to perform operations
         int resultAddition = calculator.calculate("add", 5, 3);
         int resultSubtraction = calculator.calculate("subtract", 8, 2);
         int resultMultiplication = calculator.calculate("multiply", 10, 55);
         int resultDivision = calculator.calculate("divide", 100, 9);
+        int resultModulus = calculator.calculate("mod", 25, 9);
+        
         // Display the results
         System.out.println("Result of addition: " + resultAddition);
         System.out.println("Result of subtraction: " + resultSubtraction);
         System.out.println("Result of multiplication: " + resultMultiplication);
         System.out.println("Result of division: " + resultDivision);
+        System.out.println("Results of remainder: " + resultModulus);
+
     }
 }
---
+++ Java/jmath/Modulus.java
@@ -0,0 +1,7 @@
+
+public class Modulus implements MathOperation {
+    @Override
+    public int operate(int a, int b) {
+        return a % b;
+    }
+}
