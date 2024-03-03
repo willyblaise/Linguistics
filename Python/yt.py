dde6a3e367e6a6557d1f92d7b57022ed311a0402
Willem <kravendahunter@hotmail.com>
2024-03-03 09:16:50 -0600

    adding updated script

--- Python/yt.py
+++ Python/yt.py
@@ -2,16 +2,14 @@ import pytube
 
 
 link = input("Please enter a link so we can get busy: ")
+#link = "https://www.youtube.com/watch?v=pgyt7ok7Ojg"
 yt = pytube.YouTube(link)
-
-print(yt)
-
 print("Title:", yt.title)
 print("Author:", yt.author)
 print("Published date:", yt.publish_date.strftime("%Y-%m-%d"))
 print("Number of views:", yt.views)
 print("Length of video:", yt.length, "seconds")
 
-yt.streams.get_highest_resolution().download("~/Videos")
+yt.streams.get_highest_resolution().download("/home/champ/Videos")
 
 print("Video successfullly downloaded from", link)
